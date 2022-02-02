584a4cffe7598ef49788c1375d4b81e5225c5128
Danielle Tate <danielle.tate825@gmail.com>
2022-02-02 15:30:47 -0700

    Set up sign in & sign up forms

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-import SignUp from "./pages/Explore";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<SignIn />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
